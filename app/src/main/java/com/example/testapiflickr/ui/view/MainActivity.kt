16081b7f7875d8315c003ce74c5b70e65dbe9034
xavilinkin <linkinxavi@gmail.com>
2021-12-29 00:26:37 +0100

    FEATURE: config navigation component

--- app/src/main/java/com/example/testapiflickr/ui/view/MainActivity.kt
+++ app/src/main/java/com/example/testapiflickr/ui/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.testapiflickr
+package com.example.testapiflickr.ui.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.testapiflickr.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/com/example/testapiflickr/ui/view/fragments/SearchFlickrFragment.kt
@@ -0,0 +1,22 @@
+package com.example.testapiflickr.ui.view.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.testapiflickr.R
+
+class SearchFlickrFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_search_flickr, container, false)
+    }
+}
