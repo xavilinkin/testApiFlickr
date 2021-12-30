8614f795a2127a0e72ff125f0f706644cc19529e
xavilinkin <linkinxavi@gmail.com>
2021-12-30 02:15:59 +0100

    FEATURE: add view detail - detail_fragment

--- app/src/main/java/com/example/testapiflickr/ui/view/fragments/DetailFlickFragment.kt
+++ app/src/main/java/com/example/testapiflickr/ui/view/fragments/DetailFlickFragment.kt
@@ -5,8 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
+import com.example.testapiflickr.constants.Constants
 import com.example.testapiflickr.databinding.FragmentDetailFlickBinding
+import com.squareup.picasso.Picasso
 
 class DetailFlickFragment : Fragment() {
     private var _binding: FragmentDetailFlickBinding? = null
@@ -22,8 +25,25 @@ class DetailFlickFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentDetailFlickBinding.inflate(inflater, container, false)
-        binding.testArgs.text = args.idPhoto + args.urlPhoto
+        binding.textBack.text = "Volver"
+        binding.textBack.setOnClickListener {
+            parentFragment?.findNavController()?.popBackStack()
+        }
+        setDataImage()
+        setText()
         return binding.root
+    }
+
+    private fun setText() {
+        binding.authorTextView.text = "Autor: F. Alonso"
+        binding.titleTextView.text = "Título: Fórmula 1"
+        binding.dateTextView.text = "Fecha: 18/09/1939"
+        binding.descriptionTextView.text = "Descripción: No description"
+    }
+
+    private fun setDataImage() {
+        val formatImage = args.urlPhoto + Constants.BIG_IMAGE
+        Picasso.get().load(formatImage).into(binding.imageFlickrImageView)
     }
 
     override fun onDestroy() {
